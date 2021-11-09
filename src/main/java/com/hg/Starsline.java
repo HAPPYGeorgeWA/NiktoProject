f53d3dd3cf67bbd9564d42f6304beb2933e46c10
NiktoGeorge <godspeed246@gmail.com>
2021-11-09 14:48:06 +0800

    initial commit

---
+++ src/main/java/com/hg/Starsline.java
@@ -0,0 +1,17 @@
+package com.hg;
+
+public class Starsline {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (i==j) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
