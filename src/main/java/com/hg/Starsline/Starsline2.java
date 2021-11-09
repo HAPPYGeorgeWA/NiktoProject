2949c3849e21060b5c279d920ebf78e8ef5ae98a
NiktoGeorge <godspeed246@gmail.com>
2021-11-09 15:29:28 +0800

    initial commit

--- src/main/java/com/hg/Starsline/Starsline2.java
+++ src/main/java/com/hg/Starsline/Starsline2.java
@@ -3,9 +3,9 @@ package com.hg.Starsline;
 public class Starsline2 {
     public static void main(String[] args) {
 
-        for (int i = 0; i<10; i++) {
-            for (int j = 0; j < 20; j++) {
-                if (2*i==19-j || 2*i==j) {
+        for (int i = 0; i<11; i++) {
+            for (int j = 0; j < 21; j++) {
+                if (2*i==20-j || 2*i==j) {
                         System.out.print("*");
                     } else {
                         System.out.print(" ");
