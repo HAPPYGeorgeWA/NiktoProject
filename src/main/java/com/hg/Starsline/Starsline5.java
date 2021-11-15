e9a48a08f4a78b97c4efde25ec85b326d79ba029
HAPPYGeorgeWA <godspeed246@gmail.com>
2021-11-15 22:48:47 +0800

    回字

---
+++ src/main/java/com/hg/Starsline/Starsline5.java
@@ -0,0 +1,31 @@
+package com.hg.Starsline;
+
+public class Starsline5 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 20; j++) {
+                if(i==0||i==9){
+                    System.out.print("*");
+                }
+
+                if(i>=2&i<=7){
+                    if(j==1||j==18){
+                        System.out.print(" ");
+                    }else{
+                        System.out.print("*");
+                    }
+                }
+
+                if(i==1||i==8){
+                    if(j==0||j==19){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
