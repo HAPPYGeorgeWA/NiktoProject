30d0638c0c03bd87e8409c36992ffb5a64037467
HAPPYGeorgeWA <godspeed246@gmail.com>
2021-12-07 15:28:28 +0800

    Poker

---
+++ src/main/java/com/hg/Poker.java
@@ -0,0 +1,19 @@
+package com.hg;
+
+public class Poker {
+   public static void main(String[] args) {
+      int[]cards =new int[52];
+      char[]types= {'S','H','D','C'};
+      for (int i = 0; i < 52; i++) {
+         cards[i]=i;
+      }
+
+      for (int i = 0; i < 52; i++) {
+         if(i%13==0&& i!=0){
+            System.out.println();
+         }
+         char flower = types[cards[i]/13];
+         System.out.print(cards[i]%13+1+""+flower+" ");
+      }
+   }
+}
