844b64a470a9cae58cda83a6bd89415c39f0b6dc
HAPPYGeorgeWA <godspeed246@gmail.com>
2021-12-07 15:01:14 +0800

    Dice

---
+++ src/main/java/com/hg/ArrayPractice.java
@@ -0,0 +1,14 @@
+package com.hg;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int[] n = new int[3];
+        System.out.println(n[0]);
+        float[] f =new float[3];
+        System.out.println(f[1]);
+        boolean[]b = new boolean[6];
+        System.out.println(b[2]);
+        char[]c = new char[3];
+        System.out.println(c[2]);
+    }
+}
