97f91095a3eb56fd78d532c8c92ea1f180403800
HAPPYGeorgeWA <godspeed246@gamil.com>
2021-12-14 14:06:40 +0800

    Poker version2

--- src/main/java/com/hg/Poker2.java
+++ src/main/java/com/hg/Poker2.java
@@ -5,11 +5,14 @@ public class Poker2 {
         int n = 82;
         System.out.println((char)n);
 
-
         char[]types={'c','d','h','s'};
+        String tt ="cdhs";
+        System.out.println(tt.length());
+        System.out.println(tt.charAt(1));
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 13; j++) {
-                System.out.print((j+1)+""+types[i]);
+                System.out.print((j+1)+""+tt.charAt(i));
                 System.out.print(" ");
             }
             System.out.println();
