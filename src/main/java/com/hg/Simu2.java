961ee5fcf98e5b49c2e5f5fee1f924e8701fbe2f
HAPPYGeorgeWA <godspeed246@gmail.com>
2022-01-04 15:12:23 +0800

    1/4

---
+++ src/main/java/com/hg/Simu2.java
@@ -0,0 +1,24 @@
+package com.hg;
+
+import java.util.Scanner;
+
+public class Simu2 {
+    public static void main(String[] args) {
+        int n =-1;
+        Scanner scanner = new Scanner(System.in);
+        while(n!=0){
+            System.out.println("buy item:");
+            System.out.println("1) Dress:100");
+            System.out.println("2) Sox:200");
+            System.out.println("3) Bag:300");
+            System.out.println("0) Exit");
+            String s = scanner.next();
+            n= Integer.parseInt(s);
+            int total=0;
+            int money=0;
+            if(n==1){
+
+            }
+        }
+    }
+}
