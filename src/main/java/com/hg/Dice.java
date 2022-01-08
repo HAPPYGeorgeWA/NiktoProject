178ee91cd7776b72084e27fe4699e60666dd98c9
HAPPYGeorgeWA <godspeed246@gmail.com>
2022-01-08 20:17:12 +0800

    1/8

---
+++ src/main/java/com/hg/Dice.java
@@ -0,0 +1,21 @@
+package com.hg;
+
+import java.util.Random;
+
+public class Dice {
+
+    int point =1;
+
+    public Dice(){
+        point = new Random().nextInt(6)+1;
+    }
+
+    public boolean isMax(){
+        if(point ==6){
+            return true;
+        }else{
+            return false;
+
+        }
+    }
+}
