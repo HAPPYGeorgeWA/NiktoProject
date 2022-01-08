402856a98ccfc05cf82bbfbf25a84ba3e8842163
HAPPYGeorgeWA <godspeed246@gmail.com>
2022-01-08 17:27:17 +0800

    1/8

--- src/main/java/com/hg/Simu2.java
+++ src/main/java/com/hg/Simu2.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Simu2 {
     public static void main(String[] args) {
         int n =-1;
+        int total=0;
         Scanner scanner = new Scanner(System.in);
         while(n!=0){
+            System.out.println("Please enter num:");
             System.out.println("buy item:");
             System.out.println("1) Dress:100");
             System.out.println("2) Sox:200");
@@ -14,11 +16,37 @@ public class Simu2 {
             System.out.println("0) Exit");
             String s = scanner.next();
             n= Integer.parseInt(s);
-            int total=0;
-            int money=0;
-            if(n==1){
 
+           /* switch (n){
+                case 1:
+                    total=total+100;
+                    System.out.println("Total="+total);
+                    break;
+                case 2:
+                    total=total+200;
+                    System.out.println("Total="+total);
+                    break;
+                case 3:
+                    total=total+300;
+                    System.out.println("Total="+total);
+                    break;
+                case 0:
+                    break;
+            }*/
+            if(n==1){
+                total=total+100;
+                System.out.println("Total="+total);
+            }else if(n==2){
+                total=total+200;
+                System.out.println("Total="+total);
+            }else if(n==3){
+                total =total+300;
+                System.out.println("Total="+total);
+            }else if(n==0){
+                break;
             }
         }
+        System.out.println("Thank you");
+        System.out.println("Total="+total);
     }
 }
